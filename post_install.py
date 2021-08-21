2d2cd1467ba27caf8a9ebd276855d87f3dee7e9c
jeyson <jeyson@HP-Laptop-14-cm0xxx-1f702146.localdomain>
2021-08-20 21:25:17 -0600

    GSchema support added

---
+++ post_install.py
@@ -0,0 +1,9 @@
+import subprocess
+
+schemadir = "/usr/local/share/glib-2.0/schemas"
+try:
+    print("- Compiling GSchemas...")
+    subprocess.call(['glib-compile-schemas', schemadir])
+    print("- GSchemas compiled")
+except Exception as e:
+    print("- Compilation failed...")
--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-
+import os, sys
 from distutils.core import setup
+from distutils.command.install import install as _install
+
+def _post_install(dir):
+    from subprocess import call
+    call([sys.executable, 'post_install.py'])
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, (self.install_lib,),
+                     msg="Running post install task")
 
 install_data = [
     ('share/applications', ['data/com.github.jeysonflores.hasher.desktop']),
     ('share/metainfo', ['data/com.github.jeysonflores.hasher.appdata.xml']),
     ('share/icons/hicolor/128x128/apps',['data/com.github.jeysonflores.hasher.svg']),
+    ('share/glib-2.0/schemas', ["data/com.github.jeysonflores.hasher.gschema.xml"]),
     ('bin/hasher',['data/style.css']),
     ('bin/hasher',['src/constants.py']),
-    ('bin/hasher',['src/main.py']),
+    ('bin/hasher',['src/main.py']), #/usr/share/glib-2.0/schemas
     ('bin/hasher',['src/HashView.py']),
     ('bin/hasher',['src/MainWindow.py']),
     ('bin/hasher',['src/__init__.py']),
@@ -25,5 +37,6 @@ setup(
     license='GNU GPL3',
     scripts=['com.github.jeysonflores.hasher'],
     packages=['src'],
-    data_files=install_data
+    data_files=install_data,
+    cmdclass={'install': install}
 )
